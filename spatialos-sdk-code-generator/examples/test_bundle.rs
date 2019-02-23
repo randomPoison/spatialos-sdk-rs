46b6e25d8301400639dd4e88d50a8070d67b3e38
David LeGare <dlegare.1001@gmail.com>
2019-02-23 17:12:05 -0600

    Only generate code for the current package

--- spatialos-sdk-code-generator/examples/test_bundle.rs
+++ spatialos-sdk-code-generator/examples/test_bundle.rs
@@ -1,12 +1,15 @@
+use std::str;
 
-    use std::str;
+static TEST_BUNDLE: &[u8] = include_bytes!("../data/bundle.json");
 
-    static TEST_BUNDLE: &[u8] = include_bytes!("../data/bundle.json");
-
-    fn main() {
-        let contents = str::from_utf8(TEST_BUNDLE).unwrap();
-        let bundle =
-            spatialos_sdk_code_generator::schema_bundle::load_bundle(contents).expect("Failed to parse bundle contents");
-        let generated = spatialos_sdk_code_generator::generate(bundle).expect("Code generation failed");
-        println!("{}", generated);
-    }
+fn main() {
+    let contents = str::from_utf8(TEST_BUNDLE).unwrap();
+    let bundle =
+        spatialos_sdk_code_generator::schema_bundle::load_bundle(contents).expect("Failed to parse bundle contents");
+    let example_generated = spatialos_sdk_code_generator::generate(&bundle, "example").expect("Code generation failed");
+    let improbable_generated = spatialos_sdk_code_generator::generate(&bundle, "improbable").expect("Code generation failed");
+    println!("// Generated from the improbable package.");
+    println!("{}", improbable_generated);
+    println!("// Generated from the example package.");
+    println!("{}", example_generated);
+}
--- spatialos-sdk-code-generator/src/lib.rs
+++ spatialos-sdk-code-generator/src/lib.rs
@@ -4,31 +4,38 @@ use quote::quote;
 pub mod schema_bundle;
 
 // TODO: Create a proper error type to return.
-pub fn generate(bundle: SchemaBundle) -> Result<String, Box<dyn std::error::Error>> {
-    let bundle = bundle.v1.ok_or("Only v1 bundle is supported")?;
+pub fn generate(
+    bundle: &SchemaBundle,
+    package: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let bundle = bundle.v1.as_ref().ok_or("Only v1 bundle is supported")?;
     let null_span = proc_macro2::Span::call_site();
 
     // TODO: Filter to only the components that are part of the crate that's currently
     // being processed.
-    let components = bundle.component_definitions.iter().map(|component_def| {
-        // println!("Component identifier: {:?}", component_def.identifier);
+    let components = bundle
+        .component_definitions
+        .iter()
+        .filter(|def| def.identifier.qualified_name.starts_with(package))
+        .map(|component_def| {
+            // println!("Component identifier: {:?}", component_def.identifier);
 
-        let ident = syn::Ident::new(&component_def.identifier.name, null_span);
+            let ident = syn::Ident::new(&component_def.identifier.name, null_span);
 
-        let fields = component_def.field_definitions.iter().map(|field_def| {
-            let ident = syn::Ident::new(&field_def.identifier.name, null_span);
-            let ty = &field_def.ty;
+            let fields = component_def.field_definitions.iter().map(|field_def| {
+                let ident = syn::Ident::new(&field_def.identifier.name, null_span);
+                let ty = &field_def.ty;
+                quote! {
+                    pub #ident: #ty
+                }
+            });
+
             quote! {
-                pub #ident: #ty
+                pub struct #ident {
+                    #( #fields ),*
+                }
             }
         });
-
-        quote! {
-            pub struct #ident {
-                #( #fields ),*
-            }
-        }
-    });
 
     let result = quote! {
         #( #components )*
@@ -49,6 +56,6 @@ mod tests {
         let contents = str::from_utf8(TEST_BUNDLE).unwrap();
         let bundle =
             crate::schema_bundle::load_bundle(contents).expect("Failed to parse bundle contents");
-        let _ = crate::generate(bundle).expect("Code generation failed");
+        let _ = crate::generate(&bundle, "example").expect("Code generation failed");
     }
 }
