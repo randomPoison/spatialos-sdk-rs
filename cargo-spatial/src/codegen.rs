69dc3df8564693c6eedac1e333f9be65a8f0b5b2
David LeGare <dlegare.1001@gmail.com>
2019-03-05 21:49:55 -0600

    Add build script code generation to cargo-spatial

--- cargo-spatial/src/codegen.rs
+++ cargo-spatial/src/codegen.rs
@@ -100,6 +100,36 @@ where
     Ok(())
 }
 
+/// Performs code generation for a build script.
+pub fn build() -> Result<(), Box<dyn std::error::Error>> {
+    use std::{env, fs::File};
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let bundle_path = Path::new(&out_dir).join("bundle.json");
+
+    // Use cargo-spatial to generate the schema file from all the bundles.
+    let spatial_lib_dir =
+        env::var("SPATIAL_LIB_DIR").map_err(|_| "SPATIAL_LIB_DIR environment variable not set")?;
+    compile_schemas(&spatial_lib_dir, Some(&bundle_path), None, &[])?;
+
+    // Load the source bundle.
+    let bundle_file = File::open(&bundle_path).expect("Failed to open bundle.json");
+    let bundle = serde_json::from_reader(bundle_file).expect("Failed to deserialize bundle.json");
+
+    // Use the code generator to generate the Rust types from the schema bundle.
+    let generated = spatialos_sdk_code_generator::generate(
+        &bundle,
+        "improbable",
+        &["crate::schema::improbable"],
+    )
+    .map_err(|_| "Failed to generate code from bundle")?;
+
+    let dest_path = Path::new(&out_dir).join("generated.rs");
+    fs::write(dest_path, generated).map_err(|_| "Failed to write generated code to file")?;
+
+    Ok(())
+}
+
 /// HACK: Normalizes the separators in `path`.
 ///
 /// This is necessary in order to be robust on Windows, as well as work around
